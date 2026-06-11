67b65990a373e85024711daea55d92a38f1deabc
alperenyurtseven <alperenyurtseven@papilon.com.tr>
2026-06-11 15:28:15 +0300

    feat: integrate select2 with Turkish localization globally and apply to forms

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    // Initialize Select2 globally with Turkish language and bootstrap-5 theme
+    if ($.fn.select2) {
+        $('.select2').select2({
+            theme: 'bootstrap-5',
+            language: 'tr',
+            width: '100%'
+        });
+    }
+});
